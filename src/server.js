3a7070cf78d150730edad714870dbf39e5c39093
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2021-12-10 16:15:10 +0100

    cors added

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mediaRouter from "./media/index.js";
 import listEndpoints from "express-list-endpoints";
+import cors from "cors";
 const server = express();
 const port = 3001;
+
+server.use(cors());
 server.use(express.json());
 
 server.use("/media", mediaRouter);
